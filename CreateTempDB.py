551e1bf2789801829534026e2e14d3b3607f091e
bensonmat8 <37485710+bensonmat8@users.noreply.github.com>
2021-04-03 09:03:38 -0400

    Added testing conditions and comments for all the funtions

--- CreateTempDB.py
+++ CreateTempDB.py
@@ -10,7 +10,7 @@ class Base(db.Model):
     __tablename__ = 'tbl_Base_Premium'
     Base_Premium = db.Column(db.Integer, primary_key=True)
 
-class Home_Age(db.Model):
+class HomeAge(db.Model):
     __tablename__ = 'tbl_Factor_Home_Age'
     Home_Age = db.Column(db.String(100), primary_key=True)
     Rating_Factor = db.Column(db.Float)
@@ -20,12 +20,12 @@ class Roof(db.Model):
     Roof_Type = db.Column(db.String(100), primary_key=True)
     Rating_Factor = db.Column(db.Float)
 
-class Num_Units(db.Model):
+class NumUnits(db.Model):
     __tablename__ = 'tbl_Factor_Num_Units'
     Num_Units = db.Column(db.Integer, primary_key=True)
     Rating_Factor = db.Column(db.Float)
 
-class Dwelling_Coverage(db.Model):
+class DwellingCoverage(db.Model):
     __tablename__ = 'tbl_Factor_Dwelling_Coverage'
     Dwelling_Coverage = db.Column(db.Integer, primary_key=True)
     Rating_Factor = db.Column(db.Float)
@@ -36,13 +36,13 @@ base = Base(Base_Premium=350)
 db.session.add(base)
 db.session.commit()
 
-home_age = Home_Age(Home_Age='0-10', Rating_Factor=1.0)
+home_age = HomeAge(Home_Age='0-10', Rating_Factor=1.0)
 db.session.add(home_age)
-home_age = Home_Age(Home_Age='11-35', Rating_Factor=1.5)
+home_age = HomeAge(Home_Age='11-35', Rating_Factor=1.5)
 db.session.add(home_age)
-home_age = Home_Age(Home_Age='36-100', Rating_Factor=1.8)
+home_age = HomeAge(Home_Age='36-100', Rating_Factor=1.8)
 db.session.add(home_age)
-home_age = Home_Age(Home_Age='100+', Rating_Factor=1.95)
+home_age = HomeAge(Home_Age='100+', Rating_Factor=1.95)
 db.session.add(home_age)
 db.session.commit()
 
@@ -54,26 +54,26 @@ new_roof = Roof(Roof_Type='Wood', Rating_Factor=2.0)
 db.session.add(new_roof)
 db.session.commit()
 
-num_units = Num_Units(Num_Units=1, Rating_Factor=1.0)
+num_units = NumUnits(Num_Units=1, Rating_Factor=1.0)
 db.session.add(num_units)
-num_units = Num_Units(Num_Units=2, Rating_Factor=0.8)
+num_units = NumUnits(Num_Units=2, Rating_Factor=0.8)
 db.session.add(num_units)
-num_units = Num_Units(Num_Units=3, Rating_Factor=0.8)
+num_units = NumUnits(Num_Units=3, Rating_Factor=0.8)
 db.session.add(num_units)
-num_units = Num_Units(Num_Units=4, Rating_Factor=0.8)
+num_units = NumUnits(Num_Units=4, Rating_Factor=0.8)
 db.session.add(num_units)
 db.session.commit()
 
-dwell = Dwelling_Coverage(Dwelling_Coverage=100000, Rating_Factor=0.971)
+dwell = DwellingCoverage(Dwelling_Coverage=100000, Rating_Factor=0.971)
 db.session.add(dwell)
-dwell = Dwelling_Coverage(Dwelling_Coverage=150000, Rating_Factor=1.104)
+dwell = DwellingCoverage(Dwelling_Coverage=150000, Rating_Factor=1.104)
 db.session.add(dwell)
-dwell = Dwelling_Coverage(Dwelling_Coverage=200000, Rating_Factor=1.314)
+dwell = DwellingCoverage(Dwelling_Coverage=200000, Rating_Factor=1.314)
 db.session.add(dwell)
-dwell = Dwelling_Coverage(Dwelling_Coverage=250000, Rating_Factor=1.471)
+dwell = DwellingCoverage(Dwelling_Coverage=250000, Rating_Factor=1.471)
 db.session.add(dwell)
-dwell = Dwelling_Coverage(Dwelling_Coverage=300000, Rating_Factor=1.579)
+dwell = DwellingCoverage(Dwelling_Coverage=300000, Rating_Factor=1.579)
 db.session.add(dwell)
-dwell = Dwelling_Coverage(Dwelling_Coverage=350000, Rating_Factor=1.762)
+dwell = DwellingCoverage(Dwelling_Coverage=350000, Rating_Factor=1.762)
 db.session.add(dwell)
 db.session.commit()
--- main.py
+++ main.py
@@ -16,17 +16,17 @@ def dwelling_rate(x) -> float:
     if x < 0:
         raise ValueError("Dwelling_Coverage cannot be negative")
     
-    d = Dwelling_Coverage.query.get(x)
+    d = DwellingCoverage.query.get(x)
     if d != None:
         return d.Rating_Factor
     
     if 100000 < x < 350000:
-        fltr = between(Dwelling_Coverage.Dwelling_Coverage
+        fltr = between(DwellingCoverage.Dwelling_Coverage
                     , x-50000, x+50000)
-        d = Dwelling_Coverage.query.filter(fltr)
+        d = DwellingCoverage.query.filter(fltr)
     else:
-        order_col = Dwelling_Coverage.Dwelling_Coverage
-        d = Dwelling_Coverage.query.order_by(order_col).all()
+        order_col = DwellingCoverage.Dwelling_Coverage
+        d = DwellingCoverage.query.order_by(order_col).all()
     x0 = d[0].Dwelling_Coverage
     y0 = d[0].Rating_Factor
     x1 = d[-1].Dwelling_Coverage
@@ -34,12 +34,57 @@ def dwelling_rate(x) -> float:
     result = (y0 * (x1 - x) + y1 * (x - x0)) / (x1 - x0)
     return round(result, 3)
 
+def home_age_rate(age):
+    '''
+    Takes the age, queries the 'tbl_Factor_Home_Age' table
+    and returns the rating factor
+    '''
+    if 0 <= age <= 10:
+        result = HomeAge.query.get('0-10')
+    elif 11 <= age <= 35:
+        result = HomeAge.query.get('11-35')
+    elif 36 <= age <= 100:
+        result = HomeAge.query.get('36-100')
+    else:
+        result = HomeAge.query.get('100+' )
+
+    return result.Rating_Factor
+
+def roof_type(roof):
+    '''
+    Takes the roof type, queries the 'tbl_Factor_Roof_Type' table
+    and returns the rating factor
+    '''
+    result = Roof.query.get(roof)
+    return result.Rating_Factor
+
+def num_units(units):
+    '''
+    Takes the number of units, queries the 'tbl_Factor_Num_Units'
+    table and returns the rating factor
+    '''
+    result = NumUnits.query.get(units)
+    return result.Rating_Factor
+
 def prem_quotes(j_text):
+    '''
+    Considers the base rate and all the other factors and
+    returns the premium rate rounded to two decimals.
+    '''
     j_dict = json.loads(j_text)
+    base_amt = Base.query.first().Base_Premium
+
     dwelling_factor = dwelling_rate(j_dict['DwellingCoverage'])
-    
-    
-    
+    age_factor = home_age_rate(j_dict['HomeAge'])
+    roof_factor = roof_type(j_dict['RoofType'])
+    num_units_factor = num_units(j_dict['NumberOfUnits'])
+
+    premium = base_amt * dwelling_factor * age_factor * roof_factor * num_units_factor
+
+    if j_dict['PartnerDiscount'] == 'Y':
+        premium = premium - (premium * 0.05)
+
+    return round(premium,2)    
 
 if __name__ == "__main__":
     test1 = {
@@ -51,4 +96,32 @@ if __name__ == "__main__":
         'PartnerDiscount' : 'Y'
     }
 
-    test1_json = json.dumps(test1)
+    test1 = json.dumps(test1, indent=4)
+    print(f'Test 1:\n{test1}\n\nPremium: ${prem_quotes(test1)}')
+    print('--------------------------')
+
+    test2 = {
+        'CustomerID' : 2,
+        'DwellingCoverage' : 275000,
+        'HomeAge' : 2,
+        'RoofType' : 'Wood',
+        'NumberOfUnits' : 1,
+        'PartnerDiscount' : 'Y'
+    }
+    test2 = json.dumps(test2, indent=4)
+    print(f'Test 2:\n{test2}\n\nPremium: ${prem_quotes(test2)}')
+    print('------------------------')
+
+    test3 = {
+        'CustomerID' : 3,
+        'DwellingCoverage' : 300200,
+        'HomeAge' : 108,
+        'RoofType' : 'Tin',
+        'NumberOfUnits' : 4,
+        'PartnerDiscount' : 'N'
+    }
+
+    test3 = json.dumps(test3, indent=4)
+    print(f'Test 3:\n{test3}\n\nPremium: ${prem_quotes(test3)}')
+    print('------------------------')
+    print('\n\nFor further testing, call "prem_quotes" function on the test json string.')
